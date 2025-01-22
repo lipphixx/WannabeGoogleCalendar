4e7afd764617efacfda366b37f80bbe164e99449
Ondřej Línek <LinekOndrej@litv.sssvt.cz>
2025-01-22 09:26:31 +0100

    Frontend update

--- WannabeGCalendar_Vue/WannabeGCalendar_Vue/vite.config.js
+++ WannabeGCalendar_Vue/WannabeGCalendar_Vue/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://localhost:5001',
+        changeOrigin: true,
+        secure: false, // Pokud používáte HTTPS s self-signed certifikátem
+      },
+    },
+  },
+});
